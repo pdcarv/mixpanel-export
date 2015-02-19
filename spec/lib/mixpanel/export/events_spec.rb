2b86ea6dbb6a631c34602a598bc6b86bd11d5e72
Pedro Carvalho <incude@gmail.com>
2015-02-19 15:14:10 +0000

    Spec fixes.

--- spec/lib/mixpanel/export/events_spec.rb
+++ spec/lib/mixpanel/export/events_spec.rb
@@ -24,7 +24,7 @@ describe Mixpanel::Export::Events do
       expect(events.all).to eq("")
     end
 
-    it "return a json string if request succedeed" do
+    it "return a json string if request succeeded" do
       stub_request(:get, "http://mixpanel.com/api/2.0/events").
       with(query: { api_key: 'api_key', sig: 'e720dfe014c0107e3f080b0880997bca' }).
       to_return(:status => 200, :body => response, :headers => {})
@@ -35,7 +35,15 @@ describe Mixpanel::Export::Events do
 
   describe "#top" do
     let(:response) do
-      [ 'battle','click signup button','send message','View homepage'].to_json
+      { 'events': [
+        { 'amount': 2,
+          'event': 'funnel',
+          'percent_change': -0.35635745999582824},
+        { 'amount': 75,
+          'event': 'pages',
+          'percent_change': -0.20209602478821687},
+        { 'amount': 2, 'event': 'projects', 'percent_change': 1.0 }],
+        'type': 'unique' }.to_json
     end
 
     it "returns empty if body is empty" do
@@ -46,12 +54,34 @@ describe Mixpanel::Export::Events do
       expect(events.top).to eq("")
     end
 
-    it "return a json string if request succedeed" do
+    it "return a json string if request succeeded" do
       stub_request(:get, "http://mixpanel.com/api/2.0/top").
       with(query: { api_key: 'api_key', sig: 'e720dfe014c0107e3f080b0880997bca' }).
       to_return(:status => 200, :body => response, :headers => {})
 
       expect(events.top).to eq(response)
+    end
+  end
+
+  describe "#names" do
+    let(:response) do
+      [ 'battle','click signup button','send message','View homepage'].to_json
+    end
+
+    it "returns empty if body is empty" do
+      stub_request(:get, "http://mixpanel.com/api/2.0/names").
+      with(query: { api_key: 'api_key', sig: 'e720dfe014c0107e3f080b0880997bca' }).
+      to_return(:status => 200, :body => "", :headers => {})
+
+      expect(events.names).to eq("")
+    end
+
+    it "return a json string if request succeeded" do
+      stub_request(:get, "http://mixpanel.com/api/2.0/names").
+      with(query: { api_key: 'api_key', sig: 'e720dfe014c0107e3f080b0880997bca' }).
+      to_return(:status => 200, :body => response, :headers => {})
+
+      expect(events.names).to eq(response)
     end
   end
 end
