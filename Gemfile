d9d285c8810a1070495f45a4c4160504058620aa
Pedro Carvalho <incude@gmail.com>
2015-03-17 18:12:10 +0000

    Move request to mixpanel_export directory.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 
-
 gem "httparty", '0.13.3'
 
 # Specify your gem's dependencies in mixpanel-export.gemspec
--- lib/mixpanel_export.rb
+++ lib/mixpanel_export.rb
@@ -1,4 +1,4 @@
-require "request"
+require "mixpanel_export/request"
 require "mixpanel_export/version"
 require "mixpanel_export/events"
 require "mixpanel_export/event_properties"
